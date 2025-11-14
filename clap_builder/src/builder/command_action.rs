2169c0988e13cb758eebf24566968afec34de097
Jeremiah Russell <jerry@jrussell.ie>
2025-11-14 17:29:09 +0000

    ✨ feat(clap): add `CommandAction` enum for custom command behaviors
    - introduce `CommandAction` enum to allow users to specify custom behaviors for commands
    - add `User`, `Help`, `HelpShort`, and `HelpLong` variants to `CommandAction`
    - enable feature flag `unstable-command-action` to enable `CommandAction`

---
+++ clap_builder/src/builder/command_action.rs
@@ -0,0 +1,136 @@
+
+#[cfg(debug_assertions)]
+use crate::util::AnyValueId;
+
+use crate::builder::ValueRange;
+
+/// Behavior of commands when they are encountered while parsing
+///
+/// # Examples
+///
+/// ```rust
+/// # #[cfg(feature = "help")] {
+/// # use clap_builder as clap;
+/// # use clap::Command;
+/// # use clap::Arg;
+/// let cmd = Command::new("mycmd")
+///     .subcommand(
+///         Command::new("special-help")
+///             .long("aire")
+///             .action(clap::CommandAction::Help)
+///     );
+///
+/// // Existing help still exists
+/// let err = cmd.clone().try_get_matches_from(["mycmd", "help"]).unwrap_err();
+/// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+///
+/// // New help available
+/// let err = cmd.try_get_matches_from(["mycmd", "aire"]).unwrap_err();
+/// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+/// # }
+/// ```
+#[derive(Clone, Debug, Default)]
+#[non_exhaustive]
+#[allow(missing_copy_implementations)] // In the future, we may accept `Box<dyn ...>`
+pub enum CommandAction {
+    /// When encountered, expect the user to handle the command action.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use clap_builder as clap;
+    /// # use clap::Command;
+    /// # use clap::Arg;
+    /// let cmd = Command::new("mycmd")
+    ///     .subcommands(
+    ///         Command::new("cmd")
+    ///             .long("cmd")
+    ///             .action(clap::CommandAction::User)
+    ///     );
+    ///
+    /// let matches = cmd.try_get_matches_from(["mycmd", "cmd"]).unwrap();
+    /// assert!(matches.contains_id("cmd"));
+    /// ```
+    #[default]
+    User,
+    /// When encountered, display [`Command::print_help`][super::Command::print_help]
+    ///
+    /// Depending on the flag, [`Command::print_long_help`][super::Command::print_long_help] may be shown
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[cfg(feature = "help")] {
+    /// # use clap_builder as clap;
+    /// # use clap::Command;
+    /// let cmd = Command::new("mycmd")
+    ///     .subcommand(
+    ///         Command::new("special-help")
+    ///             .long("aire")
+    ///             .action(clap::CommandAction::Help)
+    ///     );
+    ///
+    /// // Existing help still exists
+    /// let err = cmd.clone().try_get_matches_from(["mycmd", "help"]).unwrap_err();
+    /// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+    ///
+    /// // New help available
+    /// let err = cmd.try_get_matches_from(["mycmd", "aire"]).unwrap_err();
+    /// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+    /// # }
+    /// ```
+    Help,
+    /// When encountered, display [`Command::print_help`][super::Command::print_help]
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[cfg(feature = "help")] {
+    /// # use clap_builder as clap;
+    /// # use clap::Command;
+    /// # use clap::Arg;
+    /// let cmd = Command::new("mycmd")
+    ///     .subcommands(
+    ///         Command::new("special-help")
+    ///             .long("aire")
+    ///             .action(clap::CommandAction::HelpShort)
+    ///     );
+    ///
+    /// // Existing help still exists
+    /// let err = cmd.clone().try_get_matches_from(["mycmd", "help"]).unwrap_err();
+    /// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+    ///
+    /// // New help available
+    /// let err = cmd.try_get_matches_from(["mycmd", "aire"]).unwrap_err();
+    /// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+    /// # }
+    /// ```
+    HelpShort,
+    /// When encountered, display [`Command::print_long_help`][super::Command::print_long_help]
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[cfg(feature = "help")] {
+    /// # use clap_builder as clap;
+    /// # use clap::Command;
+    /// # use clap::Arg;
+    /// let cmd = Command::new("mycmd")
+    ///     .subcommands(
+    ///         Command::new("special-help")
+    ///             .long("aire")
+    ///             .action(clap::CommandAction::HelpLong)
+    ///     );
+    ///
+    /// // Existing help still exists
+    /// let err = cmd.clone().try_get_matches_from(["mycmd", "help"]).unwrap_err();
+    /// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+    ///
+    /// // New help available
+    /// let err = cmd.try_get_matches_from(["mycmd", "aire"]).unwrap_err();
+    /// assert_eq!(err.kind(), clap::error::ErrorKind::DisplayHelp);
+    /// # }
+    /// ```
+    HelpLong,
+}
+
